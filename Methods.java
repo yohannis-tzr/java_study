865d32486cfbe29760a589621f3f8218d44b8f77
yohannis-tzr <yotadzin@gmail.com>
2025-04-05 22:17:08 +0300

    method

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {;
+    public static void main(String[] args) {
+        greet("jonaw", "tad");
+        greet("john", "tad");
+    }
+    public static void greet(String firstName, String lastName){
+        System.out.println("hello, " + firstName+ " "+ lastName);
+
+    }
+}
+
+
--- arrays.java
+++ arrays.java
@@ -2,5 +2,8 @@ public class arrays {
     public static void main(String[] args) {
         String[] names = {"jonaw", "dagm", "nati"};
         System.out.println(names[2]);
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
     }
 }
